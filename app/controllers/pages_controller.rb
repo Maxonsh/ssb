f42ebbfddeb94531c44219cefd6308100ef8a5aa
Max Sh <maxim@cifronomika.ru>
2017-03-23 11:04:08 +0300

    добавил на главную списки по 3 последних предложения каждого типа

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,6 @@
 class PagesController < ApplicationController
-  def index; end
+  def main
+    @main_page_sell_offers = Sell.ordered_by_created_at_asc.last 3
+    @main_page_rent_offers = Rent.ordered_by_created_at_asc.last 3
+  end
 end
--- app/models/sell.rb
+++ app/models/sell.rb
@@ -1,2 +1,5 @@
 class Sell < Offer
+
+  scope :ordered_by_created_at_asc,  -> { order(:created_at => :asc) }
+  scope :ordered_by_created_at_desc, -> { order(:created_at => :desc) }
 end
