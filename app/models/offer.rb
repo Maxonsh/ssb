3d3ec3feebe79dd80ddd992e629d69f40f499919
Max Sh <maxim@cifronomika.ru>
2017-03-28 10:15:18 +0300

    переделал скоуп поиска по штату

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -7,14 +7,13 @@ class Offer < ApplicationRecord
   scope :ordered_by_created_at_asc,  -> { order(:created_at => :asc) }
   scope :ordered_by_created_at_desc, -> { order(:created_at => :desc) }
 
-  scope :by_state, ->(state) { where('properties.state_id = ?', state) }
+  scope :by_state, ->(state) { joins(:property).where('properties.state_id = ?', state) }
   scope :by_type, ->(type) { where('type = ?', type) }
   scope :by_price_range, lambda { |price_range|
     where('price <= :max_price AND price >= :min_price',
           :min_price => price_range.split(',')[0], :max_price => price_range.split(',')[1])
   }
 
-  validates :description, :presence => true
-  validates :price, :presence => true
+  validates :description, :price, :presence => true
   validates :price, :numericality => { :only_integer => true }
 end
