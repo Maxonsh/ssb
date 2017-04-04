4587bb39d23153e3201d0b5a760c908cbde5298c
Алексей Артамонов <alexey@cifronomika.ru>
2017-04-04 19:17:15 +0300

    добавлен футер со страницами соглашений

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
   resources :rents, :controller => 'offers', :type => 'Rent'
 
   resources :properties
+
+  get '/disclaimer', to: 'pages#disclaimer', as: :disclaimer
+  get '/contacts', to: 'pages#contacts', as: :contacts
+  get '/privacy_policy', to: 'pages#privacy_policy', as: :privacy_policy
 end
