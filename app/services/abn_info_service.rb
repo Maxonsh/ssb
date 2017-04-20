ec6f7f965c801b4ca6936a4cf736039b0c2aeed0
Max Sh <maxim@cifronomika.ru>
2017-04-20 13:46:33 +0300

    hotfix git changes

--- app/services/abn_info_service.rb
+++ app/services/abn_info_service.rb
@@ -1,7 +1,6 @@
 # class for get info about australian business number
 class AbnInfoService
   def call(abn) # rubocop:disable MethodLength
-<<<<<<< 86fc2694fd6d9d928bce45ac501a8b169e9723cf
     soap_client = Savon.client(:wsdl => Rails.application.secrets[:abn]['wsdl'],
                                :open_timeout => 5,
                                :read_timeout => 5)
@@ -12,21 +11,5 @@ class AbnInfoService
     soap_client.call(:abr_search_by_abn, :message => options).body
   rescue => e
     e.to_s
-=======
-    retries = 3
-    begin
-      soap_client = Savon.client(:wsdl => Rails.application.secrets[:abn]['wsdl'],
-                                 :open_timeout => 300,
-                                 :read_timeout => 300)
-      options = { :searchString => abn.to_s,
-                  :includeHistoricalDetails => 'n',
-                  :authenticationGuid => Rails.application.secrets[:abn]['guid'] }
-      soap_client.call(:abr_search_by_abn, :message => options).body
-    rescue => e
-      retries -= 1
-
-      retries.positive? ? retry : e.to_s
-    end
->>>>>>> добавил остальные типы объявлений, возможность выбрать период и тип в них
   end
 end
