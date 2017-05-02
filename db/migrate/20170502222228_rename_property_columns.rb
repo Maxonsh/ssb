08119e2ef53438d58c7a6004afce391e027747a7
Max Sh <maxim@cifronomika.ru>
2017-05-03 02:26:40 +0300

    поменял отображение цены

--- db/migrate/20170502222228_rename_property_columns.rb
+++
@@ -1,6 +0,0 @@
-class RenamePropertyColumns < ActiveRecord::Migration[5.0]
-  def change
-    rename_column :properties, :bed_rooms, :bedrooms
-    rename_column :properties, :bath_rooms, :bathrooms
-  end
-end
